4e65e7ef692a3c0a8698385bedf8750b7fd3ecad
mega-dean <mega_dean@fastmail.com>
2024-07-04 08:46:22 -0500

    fix js controller names

--- app/javascript/controllers/move_controller.js
+++ app/javascript/controllers/move_controller.js
@@ -2,45 +2,47 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = {
-    id: Number,
-    selectedPieceId: Number,
-    targetSquare: Number,
-    direction: String,
+    step: Object,
+    waitTime: Number,
   }
 
-  selectPiece() {
-    if (document.$('#board-grid').dataset.movesAllowedNow) {
-      if (this.selectedPieceIdValue === this.idValue) {
-        fetch(`/pieces/${this.selectedPieceIdValue}/deselect`);
-      } else {
-        this.selectedPieceIdValue = this.idValue;
-        fetch(`/pieces/${this.selectedPieceIdValue}/select`);
+  connect() {
+    new Promise((resolve) => setTimeout(resolve, this.waitTimeValue)).then(() => {
+      for (let [targetSquare, moves] of Object.entries(this.stepValue)) {
+        if (moves.captured) {
+          document.$(`#piece-${moves.captured}`)?.remove();
+        }
+        let movedPieces = (moves.moving || []).concat(moves.bumped || []);
+        movedPieces.forEach((pieceId) => {
+          this.movePieceTo(pieceId, parseInt(targetSquare));
+        });
       }
-    }
-  }
-
-  selectTarget() {
-    let spawnKind = document.$('#spawn-pieces').$('.spawn-selected')[0]?.dataset.kind;
-
-    this.postJson('/moves', {
-      piece_id: this.selectedPieceIdValue,
-      target_square: this.targetSquareValue,
-      direction: this.directionValue,
-      spawn_kind: spawnKind,
     });
+
+    [...document.$('.pending-move')].forEach((node) => node.remove());
+    document.$('#board-grid').removeAttribute('data-moves-allowed-now');
   }
 
-  postJson(url, body) {
-    var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  movePieceTo(id, dest) {
+    const piece = document.$(`#piece-${id}`);
+    const relativeDestX = dest % 8;
+    const relativeDestY = Math.floor((dest % 64) / 8);
 
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': csrfToken,
-      },
-      body: JSON.stringify(body),
-    });
+    // TODO This is duplicated in application_controller.rb.
+    const squareRem = 4;
+    const paddingRem = 0.6;
+    const boardSize = (8 * squareRem) + (2 * paddingRem);
+
+    const grid = document.$('#board-grid');
+    const destBoardIdx = Math.floor(dest / 64);
+    const destBoardX = destBoardIdx % parseInt(grid.dataset.boardsWide);
+    const destBoardY = Math.floor(destBoardIdx / parseInt(grid.dataset.boardsWide));
+    const boardXOffset = (destBoardX - parseInt(piece.dataset.boardX)) * boardSize;
+    const boardYOffset = (destBoardY - parseInt(piece.dataset.boardY)) * boardSize;
+
+    const x = boardXOffset + (squareRem * relativeDestX) + paddingRem;
+    const y = boardYOffset + (squareRem * relativeDestY) + paddingRem;
+
+    piece.style.transform = `translate(${x}rem, ${y}rem)`;
   }
-
 };
--- app/javascript/controllers/piece_controller.js
+++ app/javascript/controllers/piece_controller.js
@@ -2,47 +2,45 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = {
-    moveGeneration: Object,
-    waitTime: Number,
+    id: Number,
+    selectedId: Number,
+    targetSquare: Number,
+    direction: String,
   }
 
-  connect() {
-    new Promise((resolve) => setTimeout(resolve, this.waitTimeValue)).then(() => {
-      for (let [targetSquare, moves] of Object.entries(this.moveGenerationValue)) {
-        if (moves.captured) {
-          document.$(`#piece-${moves.captured}`)?.remove();
-        }
-        let movedPieces = (moves.moving || []).concat(moves.bumped || []);
-        movedPieces.forEach((pieceId) => {
-          this.movePieceTo(pieceId, parseInt(targetSquare));
-        });
+  selectPiece() {
+    if (document.$('#board-grid').dataset.movesAllowedNow) {
+      if (this.selectedIdValue === this.idValue) {
+        fetch(`/pieces/${this.selectedIdValue}/deselect`);
+      } else {
+        this.selectedIdValue = this.idValue;
+        fetch(`/pieces/${this.selectedIdValue}/select`);
       }
+    }
+  }
+
+  selectTarget() {
+    let spawnKind = document.$('#spawn-pieces').$('.spawn-selected')[0]?.dataset.kind;
+
+    this.postJson('/moves', {
+      piece_id: this.selectedIdValue,
+      target_square: this.targetSquareValue,
+      direction: this.directionValue,
+      spawn_kind: spawnKind,
     });
-
-    [...document.$('.pending-move')].forEach((node) => node.remove());
-    document.$('#board-grid').removeAttribute('data-moves-allowed-now');
   }
 
-  movePieceTo(id, dest) {
-    const piece = document.$(`#piece-${id}`);
-    const relativeDestX = dest % 8;
-    const relativeDestY = Math.floor((dest % 64) / 8);
+  postJson(url, body) {
+    var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-    // TODO This is duplicated in application_controller.rb.
-    const squareRem = 4;
-    const paddingRem = 0.6;
-    const boardSize = (8 * squareRem) + (2 * paddingRem);
-
-    const grid = document.$('#board-grid');
-    const destBoardIdx = Math.floor(dest / 64);
-    const destBoardX = destBoardIdx % parseInt(grid.dataset.boardsWide);
-    const destBoardY = Math.floor(destBoardIdx / parseInt(grid.dataset.boardsWide));
-    const boardXOffset = (destBoardX - parseInt(piece.dataset.boardX)) * boardSize;
-    const boardYOffset = (destBoardY - parseInt(piece.dataset.boardY)) * boardSize;
-
-    const x = boardXOffset + (squareRem * relativeDestX) + paddingRem;
-    const y = boardYOffset + (squareRem * relativeDestY) + paddingRem;
-
-    piece.style.transform = `translate(${x}rem, ${y}rem)`;
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': csrfToken,
+      },
+      body: JSON.stringify(body),
+    });
   }
+
 };
